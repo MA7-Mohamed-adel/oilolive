e22474a8346da5abe0bf71c9b4617829d7526e8e
MA7-Mohamed-adel <aahhmmed.abdo.009@gmail.com>
2025-10-31 19:57:22 +0200

    update content or design

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -108,7 +108,7 @@ export default function Product() {
             unmountOnExit
             timeout={500 * (index + 1)}
           >
-            <Grid marginLeft={3} item xs={12} sm={6} md={4} lg={2.4} key={product.id}>
+            <Grid marginLeft={2} item xs={12} sm={6} md={4} lg={2.4} key={product.id}>
             <Card
               onClick={() => navigate(`/produtdetails/${product.id}`)}
 
