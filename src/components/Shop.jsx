91918051b63c55fc0a797348471cadafe8aad462
MA7-Mohamed-adel <aahhmmed.abdo.009@gmail.com>
2025-11-04 23:24:21 +0200

    update content or design

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -94,7 +94,7 @@ const Shop = () => {
                 md={3}
                 lg={2.4}
                 key={product.id}
-                width={{xs:145,md:200}}
+                width={{xs:150,md:200}}
                 height={"100%"}
               >
                 <Card 
